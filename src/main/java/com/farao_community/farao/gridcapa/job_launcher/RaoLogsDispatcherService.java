e6d2215375007944dbe2e77b2f9d78cee7e289cb
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-01-20 16:30:11 +0100

    use task manager events model

--- src/main/java/com/farao_community/farao/gridcapa/job_launcher/RaoLogsDispatcherService.java
+++ src/main/java/com/farao_community/farao/gridcapa/job_launcher/RaoLogsDispatcherService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 
+import com.farao_community.farao.gridcapa.task_manager.api.TaskLogEventUpdate;
+
 /**
  * @author Mohamed Benrejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
  */
@@ -43,11 +45,20 @@ public class RaoLogsDispatcherService {
                 ObjectMapper objectMapper = new ObjectMapper();
                 RaoRunnerLogsModel raoLog = objectMapper.readValue(logEventString, RaoRunnerLogsModel.class);
                 if (raoLog.getClientAppId().equals(clientName)) {
-                    streamBridge.send(DESTINATION_LOGS_BINDING, raoLog);
+
+                    streamBridge.send(DESTINATION_LOGS_BINDING, convertToTaskManagerEventModel(raoLog));
                 }
             } catch (JsonProcessingException e) {
                 LOGGER.warn("parsing exception occurred while reading log event", e);
             }
         };
+    }
+
+    private TaskLogEventUpdate convertToTaskManagerEventModel(RaoRunnerLogsModel raoLog) {
+        return new TaskLogEventUpdate(raoLog.getGridcapaTaskId(),
+            raoLog.getTimestamp(),
+            raoLog.getLevel(),
+            raoLog.getMessage(),
+            raoLog.getServiceName());
     }
 }
